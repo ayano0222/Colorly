cebf172f44f785ea369c2e637823ee9a692ae1e8
ayano0222 <199382040+ayano0222@users.noreply.github.com>
2025-06-16 19:45:12 +0900

    閲覧数追加！

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_06_14_163014) do
+ActiveRecord::Schema[7.1].define(version: 2025_06_15_143329) do
   create_table "comments", force: :cascade do |t|
     t.text "comment"
     t.integer "user_id"
@@ -59,6 +59,13 @@ ActiveRecord::Schema[7.1].define(version: 2025_06_14_163014) do
     t.string "profile_image_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "view_counts", force: :cascade do |t|
+    t.integer "post_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "relationships", "users", column: "followed_id"
