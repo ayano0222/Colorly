fad727c6dba3599294bab6bc1efda33a2041a654
ayano0222 <199382040+ayano0222@users.noreply.github.com>
2025-07-07 22:28:58 +0900

    投稿時に画像も必須に

--- app/models/post.rb
+++ app/models/post.rb
@@ -14,6 +14,7 @@ class Post < ApplicationRecord
 
     validates :location, presence: true, length: { maximum: 15 }
     validates :text, presence: true, length: { maximum: 195 }
+    validates :image, presence: true
 
     enum status: { published: 0, draft: 1 }
 
